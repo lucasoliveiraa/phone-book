082ea67f6acdc35a05d19a01040e1d6d0074ac08
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-12-15 09:41:31 -0300

    ✨Create app errors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,27 @@
-import express from 'express'
-import { contactRoutes } from './routes/contact.routes';
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import { contactRoutes } from "./routes/contact.routes";
+import { AppError } from "./errors/AppError";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(contactRoutes)
+app.use(contactRoutes);
 
-app.listen(3333, () => { console.log('Server is running') });
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
+
+app.listen(3333, () => console.log("Server is running! 🚀"));
