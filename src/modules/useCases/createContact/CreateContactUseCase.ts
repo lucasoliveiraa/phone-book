fbafda03d3620149538761def892175777d5bc47
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-12-18 08:58:34 -0300

    ✨Create swagger docs

--- src/modules/useCases/createContact/CreateContactUseCase.ts
+++ src/modules/useCases/createContact/CreateContactUseCase.ts
@@ -11,11 +11,16 @@ class CreateContactUseCase {
   constructor(private contactRepository: IContactRepository) {}
 
   execute({ name, lastName, phoneNumber }: IRequest): void {
+    if (!name || !lastName || !phoneNumber) {
+      throw new AppError(
+        "Completed request with name or last name or phone number!"
+      );
+    }
     const phoneNumberAlreadyExists =
       this.contactRepository.findByPhoneNumber(phoneNumber);
 
     if (phoneNumberAlreadyExists) {
-      throw new AppError("PhoneNumber Already exists!");
+      throw new AppError("PhoneNumber already exists!");
     }
 
     this.contactRepository.create({ name, lastName, phoneNumber });
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
 import { contactRoutes } from "./routes/contact.routes";
 import { AppError } from "./errors/AppError";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(contactRoutes);
 
 app.use(
